ea5ddc60ccd3a0d30fe2b2ccb377946decfc2427
Arthur Lage <arthurlage2006@gmail.com>
2022-01-01 09:27:12 -0300

    add cors to project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
+
 const routes = require("./routes.js");
 
 require("dotenv").config();
@@ -18,5 +20,6 @@ mongoose.connect(dbUri, {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
+app.use(cors())
 
 app.listen(PORT, () => console.log("Running on port " + PORT));
